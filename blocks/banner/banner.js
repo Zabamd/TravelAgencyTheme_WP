acf2dd8a34255eaf3b0e0c2a368c0122d5c4a452
Amadeusz Zabierowski <contact@amadeuszzabierowski.net>
2023-03-16 13:10:30 +0100

    Finished banner block

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,59 +1,95 @@
-import { Button, PanelBody, PanelRow } from "@wordpress/components";
+import { Button, PanelBody, PanelRow } from '@wordpress/components';
 import {
-  InnerBlocks,
-  InspectorControls,
-  MediaUpload,
-  MediaUploadCheck,
-} from "@wordpress/block-editor";
-import { __ } from "@wordpress/i18n";
-import "./banner.scss";
+	InnerBlocks,
+	InspectorControls,
+	MediaUpload,
+	MediaUploadCheck,
+} from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import './banner.scss';
+import DefaultBannerImage from '../../assets/image/landingPagecover.png';
 
-wp.blocks.registerBlockType("travelagencytheme/banner", {
-  title: __("Banner"),
-  label: __("Banner"),
-  supports: {
-    align: ["full"],
-  },
-  attributes: {
-    align: { type: "string", default: "full" },
-  },
-  category: "travel-agency-theme",
-  edit: editComponent,
-  save: saveComponent,
-});
+wp.blocks.registerBlockType( 'travelagencytheme/banner', {
+	title: __( 'Banner' ),
+	label: __( 'Banner' ),
+	supports: {
+		align: [ 'full' ],
+	},
+	attributes: {
+		align: { type: 'string', default: 'full' },
+		imageUrl: {
+			type: 'string',
+			default: DefaultBannerImage,
+		},
+	},
+	category: 'travel-agency-theme',
+	edit: editComponent,
+	save: saveComponent,
+} );
 
-function editComponent() {
-  const BlockTemplate = [
-    ["core/paragraph", { className: "topHeading", placeholder: "Top Heading" }],
-    [
-      "core/paragraph",
-      { className: "mainHeading", placeholder: "Main Heading" },
-    ],
-    ["core/button", { className: "buttonHeading", placeholder: "Button Text" }],
-  ];
+function editComponent( { attributes, setAttributes } ) {
+	const BlockTemplate = [
+		[
+			'core/paragraph',
+			{ className: 'topHeading', placeholder: 'Top Heading' },
+		],
+		[
+			'core/paragraph',
+			{ className: 'mainHeading', placeholder: 'Main Heading' },
+		],
+		[
+			'core/button',
+			{ className: 'buttonHeading', placeholder: 'Button Text' },
+		],
+	];
 
-  return (
-    <>
-      <InspectorControls>
-        <PanelBody title={__("Background Image")} initialOpen={true}>
-          <PanelRow>
-            <MediaUploadCheck>
-              <MediaUpload />
-            </MediaUploadCheck>
-          </PanelRow>
-        </PanelBody>
-      </InspectorControls>
-      <div className="bannerEditorScreenWrapper">
-        <InnerBlocks template={BlockTemplate} templateLock="insert" />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody
+					title={ __( 'Background Image' ) }
+					initialOpen={ true }
+				>
+					<PanelRow>
+						<MediaUploadCheck>
+							<MediaUpload
+								allowedTypes={ [ 'image' ] }
+								onSelect={ ( media ) => {
+									setAttributes( { imageUrl: media.url } );
+								} }
+								render={ ( { open } ) => {
+									return (
+										<Button
+											className="menuButton"
+											onClick={ open }
+										>
+											Choose Banner Image
+										</Button>
+									);
+								} }
+							/>
+						</MediaUploadCheck>
+					</PanelRow>
+				</PanelBody>
+			</InspectorControls>
+
+			<div
+				className="bannerEditorScreenWrapper"
+				style={ { backgroundImage: `url(${ attributes.imageUrl })` } }
+			>
+				<InnerBlocks template={ BlockTemplate } templateLock="insert" />
+			</div>
+		</>
+	);
 }
 
-function saveComponent() {
-  return (
-    <div>
-      <InnerBlocks.Content />
-    </div>
-  );
+function saveComponent( { attributes } ) {
+	return (
+		<div
+			className="bannerEditorScreenWrapper"
+			style={ { backgroundImage: `url(${ attributes.imageUrl })` } }
+		>
+			<InnerBlocks.Content />
+		</div>
+	);
 }
--- build/banner.asset.php
+++ build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9e5726d9f72f256b48b4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0f9dfac6b518030a87a4');
